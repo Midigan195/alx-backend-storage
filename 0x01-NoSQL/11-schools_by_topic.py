7360eedd0e4229ab20c7b6d803bec60265a07b1e
Liam Bowers <liambowers718@gmail.com>
2024-03-25 03:07:43 -0700

    Lists all databases in MongoDB

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+'''
+This module defines a function for returning a
+list of documents that match a criteria
+'''
+
+
+def schools_by_topic(mongo_collection, topic):
+    '''Returns the list of school having a specific topic.
+    '''
+    topic_filter = {
+        'topics': {
+            '$elemMatch': {
+                '$eq': topic,
+            },
+        },
+    }
+    return [doc for doc in mongo_collection.find(topic_filter)]
